15692b7352e842c90c71d520218ca6329b2cb10d
Mike Heft <mikeheft@gmail.com>
2019-01-07 20:39:42 +0000

    Work on comments and replies with ancestry

--- app/controllers/api/v1/recipes/comments_controller.rb
+++ app/controllers/api/v1/recipes/comments_controller.rb
@@ -1,34 +1,53 @@
-class Api::V1::Recipes::CommentsController < ApiController
-  before_action :doorkeeper_authorize!
-  helper_method :recipe, :comment
-
-  def index
-    success_response(data: ::Comments::OverviewSerializer.new(recipe.comments))
-  end
-
-  def show
-    
-  end
-
-  def update
-    
-  end
-
-  def destroy
-    
-  end
-
-  private
-
-  def comment_params
-    params.require(:comment).permit(:id, :parent_id, :recipe_id, :user_id, :body)
-  end
-
-  def recipe
-    @recipe ||= Recipe.find_by(id: params[:recipe_id] || params[:id])
-  end
-
-  def comment
-    @comment ||= Comment.find_by(id: params[:comment_id] || params[:id])
+module Api
+  module V1
+    module Recipes
+      class CommentsController < ApiController
+        
+        before_action :doorkeeper_authorize!
+        helper_method :recipe, :comment
+      
+        def index
+          success_response(data: Api::V1::Comments::OverviewSerializer.new(recipe.comments.roots))
+        end
+      
+        def create
+          new_comment = recipe.comments.new(comment_params)
+          
+          if new_comment.valid?
+            new_comment.save
+      
+            success_response(data: Comments::OverviewSerializer.new(new_comment))
+          else
+            error_response(code: 404, message: t('api.comments.error'))
+          end
+        end
+      
+        def show
+          
+        end
+      
+        def update
+          
+        end
+      
+        def destroy
+          
+        end
+      
+        private
+      
+        def comment_params
+          params.require(:comment).permit(:id, :parent_id, :recipe_id, :user_id, :body)
+        end
+      
+        def recipe
+          @recipe ||= Recipe.find_by(id: params[:recipe_id] || params[:id])
+        end
+      
+        def comment
+          @comment ||= Comment.find_by(id: params[:comment_id] || params[:id])
+        end
+      end
+    end
   end
 end
