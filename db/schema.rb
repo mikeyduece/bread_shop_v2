03a328b6423bb2bc1091256aea4ac125b98ca63a
Mike Heft <mikeheft@gmail.com>
2019-01-07 19:42:04 +0000

    Add parent to comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_07_191949) do
+ActiveRecord::Schema.define(version: 2019_01_07_192641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_01_07_191949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
+    t.integer "parent_id"
     t.index ["ancestry"], name: "index_comments_on_ancestry"
     t.index ["recipe_id"], name: "index_comments_on_recipe_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
