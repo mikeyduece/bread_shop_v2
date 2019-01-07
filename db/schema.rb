3faff79ac50de530f48c9c5c0b6e0a3b4e8463a2
Mike Heft <mikeheft@gmail.com>
2019-01-07 20:50:48 +0000

    Add index to comment body

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_07_192641) do
+ActiveRecord::Schema.define(version: 2019_01_07_204906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_01_07_192641) do
     t.string "ancestry"
     t.integer "parent_id"
     t.index ["ancestry"], name: "index_comments_on_ancestry"
+    t.index ["body"], name: "index_comments_on_body"
     t.index ["recipe_id"], name: "index_comments_on_recipe_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
