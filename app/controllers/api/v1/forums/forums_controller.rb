d4bbb29998c6ff341a17efbf698723c2924744b5
Mike Heft <mikeheft@gmail.com>
2019-01-10 05:58:41 +0000

    Add forums

--- app/controllers/api/v1/forums/forums_controller.rb
+++ app/controllers/api/v1/forums/forums_controller.rb
@@ -1,15 +1,34 @@
 module Api
   module V1
     module Forums
-      class ForumsController < WebController
+      class ForumsController < ApiController
         before_action :doorkeeper_authorize!
         helper_method :forum
         def index
           # TODO: Set up filterring
+        end
 
+        def show
+          success_response(data: Forums::OverviewSerializer.new(forum))
+        end
+
+        def create
+          new_forum = Forum.new(forum_params)
+
+          if new_forum.valid?
+            new_forum.save
+
+            success_response(code: 201, data: Forums::OverviewSerializer.new(new_forum))
+          else
+            error_response(code: 500, message: t('forum.errors.create'))
+          end
         end
 
         private
+
+        def forums
+          @fourms ||= Forum.all
+        end
 
         def forum_params
           params.require(:forum).permit(:id, :title, :body, :user_id)
--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,4 +1,9 @@
 class Forum < ApplicationRecord
+  belongs_to :user
+  
   has_many :user_forum_comments, dependent: :destroy
   has_many :comments, as: :owner
+
+  validates :title, :body, presence: true
+  validates :body, length: { minimum: 10, too_short: "Comment must be at least %{count} characters" }
 end
