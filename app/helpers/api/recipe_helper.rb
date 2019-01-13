6c64ea8f60322e6a31b65fc29f2ec2a84d2b7473
Mike Heft <mikeheft@gmail.com>
2019-01-13 11:27:25 +0000

    Work on updating recipe

--- app/helpers/api/recipe_helper.rb
+++ app/helpers/api/recipe_helper.rb
@@ -8,11 +8,20 @@ module Api
 
     def create_recipe(user:, params:)
       recipe = user.recipes.create(params.except(:ingredients))
-
+require 'pry'; binding.pry
       create_recipe_ingredients(recipe: recipe, ingredients: params[:ingredients])
       recipe.recipe_ingredients.find_each(&:save)
       recipe.calculate_family
       recipe
+    end
+
+    def update_recipe(user:, ingredients:)
+      ingredients.each do |ingredient|
+        ing = Ingredient.find_by(name: ingredient[:name].downcase)
+
+        recipe_ingredient = recipe_ingredients.find_or_create_by(ingredient: ing, amount: ingredient[:amount])
+        recipe_ingredient.update_attributes(amount: ingredient[:amount])
+      end
     end
 
     private
