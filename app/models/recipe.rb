aa7a883ef7821cd82b6b337497a2983ce565e46f
Mike Heft <mikeheft@gmail.com>
2019-01-20 16:47:02 +0000

    Refactor ingredient hash

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -12,13 +12,15 @@ class Recipe < ApplicationRecord
   validates :weight_per_portion, presence: true, numericality: { greater_than: 0 }
 
   def ingredients
-    list = {}
+    list = []
     recipe_ingredients.includes(:ingredient).find_each do |recipe_ingredient|
-      list[recipe_ingredient.ingredient.name] = {
+      list << {
+        name: recipe_ingredient.ingredient.name,
         amount: recipe_ingredient.amount,
         bakers_percentage: "#{recipe_ingredient.bakers_percentage}%"
       }
     end
+
     list
   end
 
@@ -26,19 +28,27 @@ class Recipe < ApplicationRecord
     recipe_ingredients.sum('recipe_ingredients.bakers_percentage')
   end
 
-  def new_totals(params:)
-    new_total_weight = params[:number_of_portions] * params[:weight_per_portion]
-    new_flour_weight = ((new_total_weight / total_percentage) * 100).round(2)
-    scaled = {}
-    recipe_ingredients.includes(:ingredient).find_each do |recipe_ingredient|
-      new_amount = (new_flour_weight * (recipe_ingredient.bakers_percentage.to_f / 100)).round(2)
-      new_amount = new_flour_weight if recipe_ingredient.ingredient.name.eql?(:flour)
+  def new_flour_weight(params: params)
+    ((new_total_weight(params: params) / total_percentage) * 100).round(2)
+  end
 
-      scaled[recipe_ingredient.ingredient.name] = {
+  def new_total_weight(params:)
+    params[:number_of_portions] * params[:weight_per_portion]
+  end
+
+  def new_totals(params:)
+    scaled = []
+    recipe_ingredients.includes(:ingredient).find_each do |recipe_ingredient|
+      new_amount = (new_flour_weight(params: params) * (recipe_ingredient.bakers_percentage.to_f / 100)).round(2)
+      new_amount = new_flour_weight(params: params) if recipe_ingredient.ingredient.name.eql?(:flour)
+
+      scaled << {
+        name: recipe_ingredient.ingredient.name,
         amount: new_amount,
         bakers_percentage: "#{recipe_ingredient.bakers_percentage}%"
       }
     end
+    
     scaled
   end
 end
