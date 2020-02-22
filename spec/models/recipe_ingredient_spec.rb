09c9036d7280d00ef74b79ec47a7b6c363b7c402
Mike Heft <mikeheft@gmail.com>
2020-02-22 07:16:28 -0400

    Change how testing creation. As most of the methods required in the recipe and recipe ingredient creation are private.
    This now checks for callback execution over testing each method as they are majority private

--- spec/models/recipe_ingredient_spec.rb
+++ spec/models/recipe_ingredient_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe RecipeIngredient, type: :model do
   subject { create(:user) }
+  
   context :associations do
     it { belong_to(:recipe) }
     it { belong_to(:ingredient) }
+  end
+  
+  context :instance_methods do
+    it 'should callback to #ensure_bakers_percentage' do
+      recipe_ingredient = create(:recipe_ingredient)
+      expect(recipe_ingredient).to receive(:ensure_bakers_percentage)
+      recipe_ingredient.save
+    end
   end
   
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -20,31 +20,6 @@ RSpec.describe Recipe, type: :model do
   end
   
   context :instance_methods do
-    # TODO: Need to figure out how to work around callbacks, since they calcualte sums before_commit
-    xit '#flour_amts' do
-      recipe = build(:recipe)
-      
-      flour1 = create(:ingredient, name: 'ap flour')
-      flour2 = create(:ingredient, name: 'bread flour')
-      sugar = create(:ingredient, name: 'sugar')
-      
-      rec_ing1 = build(:recipe_ingredient, ingredient: flour1, amount: 100.00)
-      rec_ing2 = build(:recipe_ingredient, ingredient: flour2, amount: 300.00)
-      rec_ing3 = build(:recipe_ingredient, ingredient: sugar, amount: 0.03)
-      
-      recipe.recipe_ingredients = [rec_ing1, rec_ing2, rec_ing3]
-      recipe.save
-      
-      flour = recipe.flour_amts
-      expect(flour).to eq(400.00)
-    end
-    
-    xit 'raises NoFlourError with no flour in recipe' do
-      create(:category, name: :flour)
-      recipe = create(:recipe)
-      
-      expect { recipe.flour_amts }.to raise_error(Recipes::NoFlourError)
-    end
   end
   
 end
