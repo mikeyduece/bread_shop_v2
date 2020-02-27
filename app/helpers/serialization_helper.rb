bf53235d4393092d8d52e7f7fe19e59753bc8eaf
Mike Heft <mikeheft@gmail.com>
2020-02-27 09:12:52 -0400

    Remove serialization helper to gem

--- app/helpers/serialization_helper.rb
+++ app/helpers/serialization_helper.rb
@@ -13,14 +13,4 @@ module SerializationHelper
     { status: status, message: message }.merge(data)
   end
 
-  #def serialized_resource(resource, serializer, options = {})
-  #  return nil if resource.nil?
-  #
-  #  options[:serializer] = serializer
-  #  if resource.is_a?(Array) || resource.is_a?(::ActiveRecord::Relation)
-  #    ::ActiveModel::Serializers::CollectionSerializer.new(resource, options)
-  #  else
-  #    ::ActiveModel::Serializers::SerializableResource.new(resource, options)
-  #  end
-  #end
 end
