0d066267e85c92f470f0fcab1d3d24ccb805af72
Mike Heft <mikeheft@gmail.com>
2020-10-09 07:37:04 -0600

    Refactor users controller for fast json api

--- spec/requests/users/user_request_spec.rb
+++ spec/requests/users/user_request_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'User API' do
-  let! (:params) { {
-    "user": {
-      "first_name": "Mike",
-      "last_name": "Heft",
-      "phone_number": "+3530867806063",
-      "email": "mikeheft@gmail.com",
-      "password": "password"
+  let! (:params) do
+    {
+      data: {
+        type:       :user,
+        attributes: {
+          "first_name":   "Mike",
+          "last_name":    "Heft",
+          "phone_number": "+3530867806063",
+          "email":        "mikeheft@gmail.com",
+          "password":     "password"
+        }
+      }
     }
-  } }
+  end
+  
   let(:user) { create(:user) }
   
   context 'Authorized' do
@@ -19,10 +25,10 @@ RSpec.describe 'User API' do
       post '/api/v1/users', params: params
       
       expect(response).to be_successful
-      user_data = JSON.parse(response.body, symbolize_names: true)
+      user_data = json_response
       
-      expect(user_data[:user][:first_name]).to eq('Mike')
-      expect(User.last.first_name).to eq(params[:user][:first_name])
+      expect(user_data.dig(:data, :attributes, :first_name)).to eq('Mike')
+      expect(User.last.first_name).to eq('Mike')
     end
     
     it 'shows a Users profile information' do
@@ -31,10 +37,11 @@ RSpec.describe 'User API' do
       expect(response).to be_successful
       expect(response.status).to eq(200)
       
-      user_json = JSON.parse(response.body, symbolize_names: true)
-      expect(user_json[:user][:id]).to eq(user.id)
-      expect(user_json[:user][:first_name]).to eq(user.first_name)
-      expect(user_json[:user][:last_name]).to eq(user.last_name)
+      user_json = json_response
+      
+      expect(user_json.dig(:data, :id)).to eq(user.id.to_s)
+      expect(user_json.dig(:data, :attributes, :first_name)).to eq(user.first_name)
+      expect(user_json.dig(:data, :attributes, :last_name)).to eq(user.last_name)
     end
   end
   
