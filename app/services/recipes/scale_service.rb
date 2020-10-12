d3ebb27a05cae533f07b9f30050e2ba51acb1313
Mike Heft <mikeheft@gmail.com>
2020-10-12 07:58:42 -0600

    Refactor spec to pass

--- app/services/recipes/scale_service.rb
+++ app/services/recipes/scale_service.rb
@@ -1,5 +1,5 @@
 class Recipes::ScaleService < Recipes::BaseRecipeService
-
+  
   def call
     Success.new(scaled_recipe: scale!, scaled_dough_weight: new_total_dough_weight)
   rescue StandardError => e
@@ -11,14 +11,15 @@ class Recipes::ScaleService < Recipes::BaseRecipeService
   private
   
   attr_reader :new_total_dough_weight
-
+  
   def scale!
     new_number_of_portions = recipe_field_values(:number_of_portions)
     new_weight_per_portion = recipe_field_values(:weight_per_portion)
     @new_total_dough_weight = new_number_of_portions * new_weight_per_portion
     new_flour_weight = recipe.new_flour_weight(@new_total_dough_weight)
+    new_totals = recipe.new_totals(new_flour_weight)
     
-    recipe.new_totals(new_flour_weight)
+    new_totals.merge!({ number_of_portions: new_number_of_portions, weight_per_portion: new_weight_per_portion })
   end
   
   def recipe_field_values(attribute)
